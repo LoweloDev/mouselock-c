f72c2d4e9377d2aac2d6631b4da4406bed006a97
Tobias Barthold <tobias-barthold@outlook.de>
2023-12-03 16:10:59 +0100

    sweet spot so far

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 #include <ApplicationServices/ApplicationServices.h>
 #include <pthread.h>
 #include <stdbool.h>
+#include <stdio.h>
 
-#define BUFFER 1 // Definiert einen Pufferbereich von 1 Pixel
+#define BUFFER 10 // Definiert einen Pufferbereich von 1 Pixel
 
 bool isOutsideScreen(CGPoint location, CGRect screenBounds) {
     return location.x < screenBounds.origin.x || location.x > CGRectGetMaxX(screenBounds) - BUFFER;
@@ -13,11 +14,15 @@ void* checkMousePosition(void* arg) {
         CGPoint location = CGEventGetLocation(CGEventCreate(NULL));
         CGRect screenBounds = CGDisplayBounds(CGMainDisplayID());
 
+        printf("Mausposition: (%f, %f)\n", location.x, location.y);
+        printf("Bildschirmgrenzen: (%f, %f, %f, %f)\n", screenBounds.origin.x, screenBounds.origin.y, CGRectGetMaxX(screenBounds), CGRectGetMaxY(screenBounds));
+
         if (isOutsideScreen(location, screenBounds)) {
             CGPoint newLocation = CGPointMake(
                 fmax(screenBounds.origin.x, fmin(CGRectGetMaxX(screenBounds) - BUFFER, location.x)),
                 location.y
             );
+            printf("Neue Mausposition: (%f, %f)\n", newLocation.x, newLocation.y);
             CGEventRef moveEvent = CGEventCreateMouseEvent(NULL, kCGEventMouseMoved, newLocation, kCGMouseButtonLeft);
             CGEventPost(kCGHIDEventTap, moveEvent);
             CFRelease(moveEvent);
